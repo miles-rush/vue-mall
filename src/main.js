66e7e23b6d244e6dd0f2108f138f29ce1f868f23
Persistr <755842816@qq.com>
2020-08-07 14:12:54 +0800

    add file

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+
+
+Vue.use(ElementUI)
+
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+Vue.prototype.$http = axios
+
 
 Vue.config.productionTip = false
 
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/router/router.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const Login = () => import('../components/Login.vue')
+const Home = () => import('../components/Home.vue')
+const Welcome = () => import('../components/Welcome.vue')
+const Users = () => import('../components/user/Users.vue')
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    {
+      path:'/',
+      redirect:'/login'
+    },
+    {
+      path:'/login',
+      component:Login
+    },
+    {
+      path:'/home',
+      component:Home,
+      redirect:'/welcome',
+      children: [
+        {
+          path:'/welcome',
+          component:Welcome
+        },
+        {
+          path:'/users',
+          component:Users
+        }
+      ]
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.path === '/login') return next();
+  const tokenStr = window.sessionStorage.getItem('token')
+  if(!tokenStr) return next('/login')
+  next()
+})
+
+
+export default router
