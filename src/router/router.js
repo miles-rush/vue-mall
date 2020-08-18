13f70c0ddc45c3f8f73342b99f7f00a23155156d
Persistr <755842816@qq.com>
2020-08-18 13:12:44 +0800

    完成了报表功能的开发

--- src/router/router.js
+++ src/router/router.js
@@ -12,6 +12,7 @@ const Params = () => import('../components/goods/Params.vue')
 const List = () => import('../components/goods/List.vue')
 const Add = () => import('../components/goods/Add.vue')
 const Order = () => import('../components/order/Order.vue')
+const Report = () => import('../components/report/Report.vue')
 
 Vue.use(Router)
 
@@ -64,6 +65,9 @@ const router = new Router({
         },{
           path:'/orders',
           component:Order
+        },{
+          path:'/reports',
+          component:Report
         }
       ]
     }
